66a4738ac0efd1970c895a2bfc744e517d5dd50b
Loolin Teo <loolinn@Loolins-MacBook-Air.local>
2017-10-18 23:35:27 +0800

    first commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Loolin Teo on 18/10/17.
+//  Copyright © 2017 Chris Loo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
